cd9d8aa3ba76e33b5384d12e16f90101724d6c15
Veronika050987 <veronicaviv@mail.ru>
2025-11-28 20:11:42 +0300

    data binding DONE

--- Binding/MainWindow.xaml.cs
+++ Binding/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,26 +13,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
 
 namespace Binding
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
 		public MainWindow()
 		{
+			DataContext = this;
 			InitializeComponent();
+			txtInput.Focus();
 		}
-
+		public event PropertyChangedEventHandler PropertyChanged;
 		private string boundText;
 
 		public string BoundText
 		{
 			get { return boundText; }
-			set { boundText = value; }
+			set 
+			{ 
+				boundText = value;
+				//PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoundText)));
+				OnPropertyChanged(nameof(BoundText));
+			}
 		}
-
+		void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		private void btnSet_Click(object sender, RoutedEventArgs e)
+		{
+			BoundText = "Set from button";
+		}
 	}
 }
